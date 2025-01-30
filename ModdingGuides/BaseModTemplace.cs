cb8c199b583fdc3e25b7d8cdcf4e158b555be876
NgbatzYT <94680235+NgbatzYT@users.noreply.github.com>
2025-01-30 20:42:58 +1100

    Create BaseModTemplace.cs

---
+++ ModdingGuides/BaseModTemplace.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using EscapePoland.Mods;
+
+namespace YourFirstMod
+{
+    public class BaseMod : ModHandler
+    {
+        
+        // Run when the game starts
+        public void Start()
+        {
+            
+        }
+        
+        // Runs when a level gets loaded
+        public void OnLevelLoaded()
+        {
+            
+        }
+        
+        // Runs every frame that the game runs
+        public void Update()
+        {
+            
+        }
+    }
+} 
